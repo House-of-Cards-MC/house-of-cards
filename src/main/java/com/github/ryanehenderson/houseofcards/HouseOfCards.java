5582f7f97164ccb1911c718dcd0056a3404c41c5
Ryan Henderson <ryan.ev.henderson@gmail.com>
2021-01-05 15:04:15 -0600

    Initial mod structure setup

--- src/main/java/com/github/ryanehenderson/houseofcards/HouseOfCards.java
+++ src/main/java/com/github/ryanehenderson/houseofcards/HouseOfCards.java
@@ -1,1 +1,39 @@
-package com.github.ryanehenderson.house-of-cards;
+package com.github.ryanehenderson.houseofcards;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("houseofcards")
+public class HouseOfCards {
+    private static HouseOfCards instance;
+    private static final String modId = "houseofcards";
+    private static final Logger logger = LogManager.getLogger(modId);
+
+    public HouseOfCards() {
+        instance = this;
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientRegistries);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        logger.info("Setup method registered");
+    }
+
+    private void clientRegistries(final FMLClientSetupEvent event) {
+        logger.info("clientRegistries method registered");
+    }
+
+    public static HouseOfCards get() {
+        return instance;
+    }
+
+    private String getModId() {
+        return modId;
+    }
+}
